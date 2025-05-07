7a5708d96a4b143fda3a7e04c4e8ef7a2c962f2d
pavitra-pandith123 <pavitrapandith595@gmail.com>
2025-05-07 15:06:35 +0530

    save

---
+++ Employee/employee.java
@@ -0,0 +1,24 @@
+package Employee;
+
+public class employee {
+    String name;
+    int id;
+    double salary;
+
+    public employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        employee e = new employee("abc", 123, 5000);
+        e.displayInfo();
+    }
+}
