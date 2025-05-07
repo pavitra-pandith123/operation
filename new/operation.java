c0b5b256151ef9d193d29529b5006bde1d0aa136
pavitra-pandith123 <pavitrapandith595@gmail.com>
2025-05-07 15:28:24 +0530

    save

---
+++ new/operation.java
@@ -0,0 +1,10 @@
+public class operation {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+    }
+    
+}
