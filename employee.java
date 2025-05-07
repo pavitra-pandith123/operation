10f6d2bbf11b9b87f9a7bd1ef836c2750c9d9870
pavitra-pandith123 <pavitrapandith595@gmail.com>
2025-05-07 15:08:28 +0530

    save

--- employee.java
+++ employee.java
@@ -4,21 +4,25 @@ public class employee {
     String name;
     int id;
     double salary;
+    String add;
 
-    public employee(String name, int id, double salary) {
+    public employee(String name, int id, double salary,String add) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.add=add;
     }
 
     public void displayInfo() {
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Salary: " + salary);
+        System.out.println("addr" + add);
+
     }
 
     public static void main(String[] args) {
-        employee e = new employee("abc", 123, 5000);
+        employee e = new employee("abc", 123, 5000,"hubli");
         e.displayInfo();
     }
 }
